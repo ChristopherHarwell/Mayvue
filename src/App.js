033b5d2e56b585da2f5ef1a236b8c8baa9667a8b
Chris <chrisharwell72@gmail.com>
2020-06-03 10:59:24 -0400

    finished project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [textValue, setTextValue] = useState({
+    text: "",
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  const handleChange = (e) => {
+    e.persist();
+
+    setTextValue({
+      [e.target.name]: e.target.value,
+    });
+    console.log(textValue);
+  };
+
+  // textbox styling
+  let inputStyle = {
+    border: "1px solid black",
+  };
+
+  // input border conditional styling
+  if (textValue.text.length > 20) {
+    inputStyle = {
+      border: "3px solid red",
+    };
+  }
+
+  // number of button clicks
+  const [buttonClicks, setButtonClicks] = useState(0);
+
+  const numButtonClicks = () => {
+    return setButtonClicks(buttonClicks + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <label>{textValue.text}</label>
+        <input
+          style={inputStyle}
+          name="text"
+          onChange={handleChange}
+          type="textbox"
+        />
+      </form>
+      <button onClick={numButtonClicks}>Click Me</button>
+      <label> Number of button clicks: {buttonClicks}</label>
     </div>
   );
 }
